0c706c99fdaa6ab69e7d9196ae1a971b4eeac791
KaiN <kain@piwnica.ws>
2022-10-09 11:01:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Chaos Arena
+
+A small multiplayer game for OCS Amiga machines.
+
+Controls:
+
+- up to 2 players on regular joy ports
+- up to 2 players on parallel-to-joy adapter
+- up to 2 players on keyboard:
+  - movement: <kbd>&uarr;</kbd> <kbd>&darr;</kbd> <kbd>&larr;</kbd> <kbd>&rarr;</kbd>, attack: <kbd>Right Shift</kbd>
+  - movement: <kbd>W</kbd> <kbd>S</kbd> <kbd>A</kbd> <kbd>D</kbd>, attack: <kbd>Left Shift</kbd>
+
+Credits:
+
+- Code: [KaiN](https://github.com/tehKaiN)
+- Character sprites from [Shade's Puny Characters pack (CC0)](https://merchant-shade.itch.io/16x16-puny-characters)
+- Game mechanics based on Chaos Castle event from Mu Online game by Webzen corp.
---
+++ src/chaos_arena.c
@@ -0,0 +1,36 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "chaos_arena.h"
+#include <ace/generic/main.h>
+#include <ace/managers/key.h>
+#include <ace/managers/joy.h>
+#include <ace/managers/state.h>
+#include <ace/managers/ptplayer.h>
+
+tStateManager *g_pStateMachineGame;
+
+void genericCreate(void) {
+	g_pStateMachineGame = stateManagerCreate();
+	keyCreate();
+	joyOpen();
+	ptplayerCreate(1);
+	// assetsGlobalCreate();
+	// statePush(g_pStateMachineGame, &g_sStateLogo);
+}
+
+void genericProcess(void) {
+	ptplayerProcess();
+	keyProcess();
+	joyProcess();
+	// stateProcess(g_pStateMachineGame);
+}
+
+void genericDestroy(void) {
+	ptplayerDestroy();
+	// assetsGlobalDestroy();
+	keyDestroy();
+	joyClose();
+	// stateManagerDestroy(g_pStateMachineGame);
+}
